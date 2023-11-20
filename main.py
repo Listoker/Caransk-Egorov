11c0297baf9460f1a177bacd398a1e7b92661a8d
Yandex_Lyceum_D2 <egor57557@gmail.com>
2023-11-20 19:58:33 +0300

    second круги2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtGui import QPainter, QColor
 import random
 
 
