cbfaee1c599432ae4882fa6e1489cd294be8e50e
Yandex_Lyceum_D2 <egor57557@gmail.com>
2023-11-20 19:22:29 +0300

    second круги

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QPainter, QColor, QFont
+import random
 
 
-# Press the green button in the gutter to run the script.
+class Arifmometr(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        self.setGeometry(300, 150, 500, 150)
+        self.setWindowTitle('круг')
+
+        self.add_button = QPushButton('круг', self)
+        self.add_button.resize(50, 50)
+        self.add_button.move(50, 0)
+        self.add_button.clicked.connect(self.hello)
+
+    def hello(self):
+        print(123)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# dencehbbhededed
+    app = QApplication(sys.argv)
+    example = Arifmometr()
+    example.show()
+    sys.exit(app.exec())
